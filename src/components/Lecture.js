969347362534a3c121f035aab63bd55b1b578914
chenchiuchi <60934682+chenchiuchi@users.noreply.github.com>
2023-08-12 00:43:25 +0800

    tune

--- src/components/Lecture.js
+++ src/components/Lecture.js
@@ -1,25 +1,26 @@
 import LectureClass from "./LectureClass";
-import './Lecture.css';
+import "./Lecture.css";
 
 function Lecture(props) {
-
-    return (
-        <div className="expenses">
-            <LectureClass
-            title={props.data[0].title} 
-            amount = {props.data[0].amount}
-            date = {props.data[0].date}></LectureClass>
-            <LectureClass
-            title={props.data[1].title} 
-            amount = {props.data[1].amount}
-            date = {props.data[1].date}></LectureClass>
-            <LectureClass
-            title={props.data[1].title} 
-            amount = {props.data[1].amount}
-            date = {props.data[1].date}></LectureClass>
-
-        </div>
-    );
+  return (
+    <div className="expenses">
+      <LectureClass
+        title={props.data[0].title}
+        amount={props.data[0].amount}
+        date={props.data[0].date}
+      ></LectureClass>
+      <LectureClass
+        title={props.data[1].title}
+        amount={props.data[1].amount}
+        date={props.data[1].date}
+      ></LectureClass>
+      <LectureClass
+        title={props.data[1].title}
+        amount={props.data[1].amount}
+        date={props.data[1].date}
+      ></LectureClass>
+    </div>
+  );
 }
 
-export default Lecture; 
+export default Lecture;
